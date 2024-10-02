e1ddeb44a6a6463ab67edd2d17ab1e5d34e3cad2
taxel55 <taxel2023@gmail.com>
2024-10-02 23:38:46 +0700

    changes in main config file

--- dexterstore/tailwind.config.js
+++ dexterstore/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js,vue,jsx,tsx}", // Add more file types or directories as needed
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#1A1C22',
+        'accent': '#580EF6',
+        'text': '#F4F5FC',
+        'secondary': '#3D3A50',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    // Add Tailwind plugins as needed (e.g., require('tailwindcss/forms'))
+  ],
 }
-
